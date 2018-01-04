21e55e7e2841e1b50113a8f33e12d1b595e37c57
Pankaj Prakash <pankaj.prakash@vasudhaika.net>
2018-01-04 23:49:45 +0530

    Added atoi.c

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# Codeforwin-C-programming
+# Codeforwin C programming
 Codeforwin is a platform for learning, practicing and sharing coding skills. 
+
+A blog about computer science, for computer science learner – by computer science lover.
---
+++ stdlib/atoi.c
@@ -0,0 +1,36 @@
+/**
+ * @author          Pankaj Prakash
+ * @website         http://codeforwin.org/
+ * @lastmodified    11:35 AM, January 5, 2018
+ * @description     C programt to convert string to integer using atoi library function.
+ *                  
+ *                  Syntax - int atoi (const char * str); 
+ * ------------------------------------------------------------------------------------- */
+
+#include <stdio.h>
+#include <stdlib.h>     // Used for atoi()
+
+#define INTEGER_SIZE 20 // Maximum size of integer in string representation
+
+
+int main()
+{
+    char number[20];
+    int  num;
+
+
+    /* Input string representation of integer from user. */
+    printf("Enter any integer: ");
+    gets(number);
+
+
+    /* Convert string representation of number to integer */
+    num = atoi(number);
+
+
+    /* Print converted integer */
+    printf("Converted integer = %d\n", num);
+
+
+    return 0;
+}
